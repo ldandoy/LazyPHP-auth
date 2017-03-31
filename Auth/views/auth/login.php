b9d4bde6510f8728ddcd8cb41bd76dd39641605d
ldandoy <ldandoy@overconsulting.net>
2017-03-31 15:09:23 +0200

    Ajout CSS Login

--- Auth/views/auth/login.php
+++ Auth/views/auth/login.php
@@ -1,9 +1,16 @@
-<h1 class="page-title">{{ pageTitle }}</h1>
-<form id="formLogin" method="post" action="<?php echo $params['formAction']; ?>" class="form form-horizontal">
-    {% input_text name="email" model="email" label="Identifiant" %}
-    {% input_password name="password" model="password" value="" label="Mot de passe" autocomplete="off" %}
-    {% input_submit name="submit" value="login" formId="formLogin" label="Se connecter" class="btn-primary" %}
-</form>
-<p>
-	Pas encore de compte? {% link url="user_signup" content="S'inscrire" new_window="1" %}
-</p>
+<div class="login-box">
+	<div class="login-box-logo">
+		<b>Admin</b>LTE
+	</div>
+	<div class="login-box-body">
+		<form id="formLogin" method="post" action="<?php echo $params['formAction']; ?>" class="form form-horizontal">
+			<p class="page-title">{{ pageTitle }}</p>
+		    {% input_text name="email" model="email" placeholder="Identifiant" %}
+		    {% input_password name="password" model="password" value="" placeholder="Mot de passe" autocomplete="off" %}
+		    {% input_submit name="submit" value="login" formId="formLogin" label="Se connecter" class="btn-primary" %}
+		</form>
+		<p>
+			Pas encore de compte? {% link url="user_signup" content="S'inscrire" new_window="1" %}
+		</p>
+	</div>
+</div>
