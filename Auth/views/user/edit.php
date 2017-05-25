c31e0c637dbbaca677f3e9bb64962497dd6eb62d
ldandoy <ldandoy@overconsulting.net>
2017-05-25 18:39:22 +0200

    -m

--- Auth/views/user/edit.php
+++ Auth/views/user/edit.php
@@ -1,21 +1,27 @@
-<h1 class="page-title">Modification de vos données personelles</h1>
-<div class="box box-success">
-    <div class="box-header">
-        <div class="box-tools pull-right">
-            {% button url="user_index" type="default" icon="arrow-left" class="btn-xs" %}
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="page-title">Modification de vos données personelles</h1>
+            <div class="box box-success">
+                <div class="box-header">
+                    <div class="box-tools pull-right">
+                        {% button url="user_index" type="default" icon="arrow-left" class="btn-xs" %}
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="box-body">
+            {% form_open id="formUser" action="formAction" class="form-horizontal" %}
+                {% input_text name="lastname" model="user.lastname" label="Nom" %}
+                {% input_text name="firstname" model="user.firstname" label="Prénom" %}
+                {% input_text name="email" model="user.email" label="Email" %}
+                {% input_textarea name="address" model="user.address" label="Adresse" %}
+                {% input_password name="newPassword" model="user.newPassword" label="Nouveau mot de passe" autocomplete="off" %}
+                {% input_media name="media_id" model="user.media_id" label="Image" mediaType="Image" mediaCategory="user" %}
+                {% input_checkbox name="active" model="user.active" label="Actif" %}
+                {% input_submit name="submit" value="save" formId="formUser" class="btn-primary" icon="save" label="Enregistrer" %}
+            {% form_close %}
+            	</div>
+            </div>
         </div>
-        <div class="clearfix"></div>
     </div>
-    <div class="box-body">
-{% form_open id="formUser" action="formAction" class="form-horizontal" %}
-    {% input_text name="lastname" model="user.lastname" label="Nom" %}
-    {% input_text name="firstname" model="user.firstname" label="Prénom" %}
-    {% input_text name="email" model="user.email" label="Email" %}
-    {% input_textarea name="address" model="user.address" label="Adresse" %}
-    {% input_password name="newPassword" model="user.newPassword" label="Nouveau mot de passe" autocomplete="off" %}
-    {% input_media name="media_id" model="user.media_id" label="Image" mediaType="Image" mediaCategory="user" %}
-    {% input_checkbox name="active" model="user.active" label="Actif" %}
-    {% input_submit name="submit" value="save" formId="formUser" class="btn-primary" icon="save" label="Enregistrer" %}
-{% form_close %}
-	</div>
 </div>
--- Auth/views/user/index.php
+++ Auth/views/user/index.php
@@ -1,10 +1,16 @@
-<h1 class="page-title">Votre compte</h1>
-<div class="box box-success">
-    <div class="box-header">
-    	<h2><img src="{{user.getImageUrl}}" />{{user.getFullname}}</h2>
-    </div>
-    <div class="box-body">
-        {% link url="user_edit" content="Modifier vos données personnelles" icon="pencil" %}<br />
-		{% link url="auth_logout" content="Se déconnecter" icon="sign-out" %}
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+			<h1 class="page-title">Votre compte</h1>
+			<div class="box box-success">
+			    <div class="box-header">
+			    	<h2><img src="{{user.getImageUrl}}" />{{user.getFullname}}</h2>
+			    </div>
+			    <div class="box-body">
+			        {% link url="user_edit" content="Modifier vos données personnelles" icon="pencil" %}<br />
+					{% link url="auth_logout" content="Se déconnecter" icon="sign-out" %}
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
