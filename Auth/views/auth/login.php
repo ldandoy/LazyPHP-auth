59b1926a2b1cffc5058fdd9872a9f4fa21289f81
ldandoy <ldandoy@overconsulting.net>
2017-05-30 16:01:07 +0200

    -m

--- Auth/views/auth/login.php
+++ Auth/views/auth/login.php
@@ -9,9 +9,12 @@
 		<form id="formLogin" method="post" action="<?php echo $params['formAction']; ?>" class="form form-horizontal">
 			<p class="page-title">{{ pageTitle }}</p>
 			{% input_text name="email" model="email" label="Email" placeholder="Identifiant" %}
-			{% input_password name="password" label="Password" model="password" value="" placeholder="Mot de passe" autocomplete="off" help="Mot de passe oublié? <a href="forgotpassword">Cliquez ici</a>" %}
+			{% input_password name="password" label="Password" model="password" value="" placeholder="Mot de passe" autocomplete="off" %}
 			{% input_submit name="submit" value="login" formId="formLogin" label="Se connecter" class="btn-primary" %}
 		</form>
+		<p>
+			Mot de passe oublié? <a href="forgotpassword">Cliquez ici</a>
+		</p>
 		<p>
 			Pas encore de compte? {% link url="<?php echo $params['signupURL']; ?>" content="S'inscrire" %}
 		</p>
