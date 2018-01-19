05844fbde1bd7b98f54325ddf461a2b57578e799
Laurent Comex <laurent.comex@sfr.fr>
2018-01-19 10:46:33 +0100

    maj

--- Auth/views/user/index.php
+++ Auth/views/user/index.php
@@ -1,37 +1,71 @@
+<?php $url = $user->media != null ? $user->media->getUrl() : '';
+    if ($url != '') {
+        // $avatar = '{% image src="'.$url.'" width="150px" height="150px" %}';
+        $avatar = '<img src="'.$url.'"  class="rounded-circle" />';
+    } else {
+        $avatar = '<i class="fa fa-user"></i>';
+    }
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
 			<h1 class="page-title">Votre compte</h1>
 			<div class="box box-success">
-			    <div class="box-header">
-			    	<h2><img src="{{user.getImageUrl}}" />{{user.getFullName}}</h2>
-			    </div>
 
-<?php
-				foreach ($orders as $order) {
-                             foreach ($order->orderdetails as $orderdetail) {
-                                 $label = $orderdetail->product->label;
-                                 $amount = $orderdetail->amount;
-                                 $quantity = $orderdetail->quantity ;
-                                        }  
-                                echo
-                                '<tr>
-                                    <td>'. $order->user->fullname .'</td>
-                                    <td>'. $label .'</td>
-                                    <td>'. number_format(round($amount, 2),2) .' euros </td>  
-                                    <td>'. $quantity .'</td>     
-                                <td>';
-                                echo
-                                    '</td>'.
-                                '</tr> <br />';   
-                        }
-?>
+			    <div class="box-infos">
+					<h3>Informations personelles</h3>
+		            <div class="card slot-coach">
+		                    <div>
+		                        <?php echo $avatar; ?>
+		                    </div>
 
-			    <div class="box-body">
+		                    <div>
+		                        <strong><?php echo $user->fullname ; ?></strong>
+		                        <br /><?php echo $user->email; ?> 
+		                    </div>
+		                </div>
+		            </div>     
+		         </div>
 
+				<div class="box-orders">
+					<h3>Commandes</h3>
+		            <div class="card">
+						<?php
+						foreach ($orders as $order) {
+		                             foreach ($order->orderdetails as $orderdetail) {
+		                                 $label = $orderdetail->product->label;
+		                                 $label_slot = $orderdetail->product->label_slot;
+		                                 $amount = $orderdetail->amount;
+		                                 $quantity = $orderdetail->quantity ;
+		                                        }  
+		                                echo
+		                                '<div class="slot-order">
+		                                <table>
+			                                <tr>
+			                                    <td>'. $order->user->fullname . /* nom du coach */'</td> 
+			                                    <td>'. $label_slot ./* Sous catégorie */'</td>
+			                                    <td>'. $label ./* catégorie */'</td>
+			                                    <td>'. number_format(round($amount, 2),2) . /* Prix */' euros </td>  
+			                                    <td>'. $quantity . /* quantité */ '</td>     
+				                                <td>';
+				                                ?>
+				                                {% button url="cockpit_orders_delete_<?php echo $order->id; ?>" type="danger" size="sm" icon="trash-o" confirmation="Vous confirmer vouloir supprimer cette commande ?" hint="Supprimer" %}
+			                                <?php
+			                                echo
+			                                    '</td>'.
+			                                '</tr> 
+		                                </table>
+		                                <br /></div>';   
+		                        }
+						?>
+					</div>
+				 </div>
+
+			 	<div class="box-body">
 			        {% link url="user_edit" content="Modifier vos données personnelles" icon="pencil" %}<br />
 					{% link url="auth_logout" content="Se déconnecter" icon="sign-out" %}
 				</div>
+
 			</div>
 		</div>
 	</div>
